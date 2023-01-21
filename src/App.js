d36eb18f38201ddf9fa69bf2259427fc6043f036
crestrada357 <crestrada357@gmail.com>
2023-01-21 15:16:22 -0500

    Minor change in App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './styles/App.css';
 import { AppUI } from './AppUI';
 
-const defaultTodos=[
-  {key: 1, text:"Cut onions", completed:true},
-  {key: 2, text:"Grate cheese", completed:false},
-  {key: 3, text:"Make dessert", completed:false},
-  {key: 4, text:"Learn react", completed:false}
-];
+const localStorageTodos = localStorage.getItem('TODOS');
+let parsedTodos;
+
+if(!localStorageTodos){
+  localStorage.setItem('TODOS', JSON.stringify([]));
+  parsedTodos=[];
+}else{
+  parsedTodos = JSON.parse(localStorageTodos);
+}
 
 function App() {
-  const [todos, setTodos]= React.useState(defaultTodos);
+  const [todos, setTodos]= React.useState(parsedTodos);
   const [searchValue, setSearchValue]= React.useState('');
   const completedTodos=todos.filter(todo=>!!todo.completed).length;
   const totalTodos=todos.length;
@@ -26,17 +29,24 @@ function App() {
     });
 
   }
+
+  const saveTodos = (newTodos)=>{
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS', stringifiedTodos);
+    setTodos(newTodos);
+  }
+
   const completeTodo=(text)=>{
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos=[...todos];
     newTodos[todoIndex].completed=true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
   const deleteTodo=(text)=>{
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos=[...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
   return (
     <AppUI
